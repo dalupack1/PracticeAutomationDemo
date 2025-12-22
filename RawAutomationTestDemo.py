f9a981a387f725b59e0f8f512813ee863a0f5306
DaluPack1 <Dalupackqa@gmail.com>
2025-12-22 01:20:52 -0500

    Raw Automation Script

--- RawAutomationTestDemo.py
+++ RawAutomationTestDemo.py
@@ -38,11 +38,16 @@ sleep(2)
 
 assert 'https://practicetestautomation.com/logged-in-successfully/' in driver.current_url
 sleep(1)
+print("Test Passed: Correct URL")
 
 logged_in_successfully_message = driver.find_element(By.XPATH, '//h1[@class="post-title"]')
 assert logged_in_successfully_message.text == 'Logged In Successfully'
 sleep(1)
+print(("Test Passed: Successfully logged in"))
 
 logout_button = driver.find_element(By.CLASS_NAME, 'wp-block-button__link')
 assert logout_button.text == 'Log out', f'Log out button not visible: {logout_button.text}'
 sleep(1)
+print("Test Passed: Log out button available")
+driver.close()
+driver.quit()
